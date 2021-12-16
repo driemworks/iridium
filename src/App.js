7e789f31de7f511b47bb593eacbcc1ce96b02582
driemworks <tonyrriemer@gmail.com>
2021-12-16 11:22:26 -0600

    update css

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
             <p>
               The steps to run Iris node and UI is outlined in chapter two of our documentation.
             </p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/ATxOtEnFmTU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe className='video-container' src="https://www.youtube.com/embed/ATxOtEnFmTU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
         </div>
       </div>
